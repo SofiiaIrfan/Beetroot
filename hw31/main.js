e01e556a3f0474df2d0c6376d5edd271b9532fa8
SofiiaIrfan <58609288+SofiiaIrfan@users.noreply.github.com>
2023-03-14 14:06:08 +0100

    Add files via upload

---
+++ hw31/main.js
@@ -0,0 +1,75 @@
+function guessGame() {
+    let userNumber = alert('Think of a number between 0 and 100!'); 
+    let guess = prompt('Is your number > 50 < 50 or == 50 ? Enter: <, > or ==:');
+    let min = 0;                             
+    let middle = 50;                          
+    let max = 100;                              
+    while (guess != '==') {    
+        if (guess == '>') {                        
+            min = middle;                      
+            range = max - min;                     
+            rangeHalf = range / 2;                  
+            rangeHalf = Math.floor(range / 2);      
+            let center = min + rangeHalf;           
+            middle = center;   
+            let guessAgain = prompt('Is your number > ' + middle + ' < ' + middle + ' or == ' + middle + ' ? Enter: <, > or ==:'); 
+            guess = guessAgain;
+        }
+        if (guess == '<') {                                
+            max = middle                               
+            range = max - min;                             
+            rangeHalf = range / 2;                         
+            rangeHalf = Math.floor(range / 2);             
+            let center = min + rangeHalf;                   
+            middle = center;
+            let guessAgain = prompt('Is your number > ' + middle + ' < ' + middle + ' or == ' + middle + ' ? Enter: <, > or ==:'); 
+            guess = guessAgain;
+        }
+    }    
+    answer = alert('Your number was: ' + middle + '!!')
+}
+
+
+function multiplicationTableBuilder(min = 1, max = 10) {
+    let row = "";
+    let arr = [];
+    for (let i = min; i <= max; i++) {
+      for (let j = min; j <= max; j++) {
+        row += `${i}*${j}=${+i * +j} `;
+      }
+  
+      arr.push(row);
+      // console.log(arr);
+      row = "";
+    }
+    return arr;
+  }
+  
+
+  
+  function dateCalculation(year = "2022", month = "12", day = "12", add = "1") {
+    year = Number(year);
+    month = Number(month) - 1;
+    day = Number(day);
+    add = Number(add);
+  
+    let userDate = new Date(year, month, day);
+    userDate.setDate(userDate.getDate() + add);
+  
+    const dateOptions = {
+      era: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      weekday: "long",
+      timezone: "UTC",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+  
+    return userDate.toLocaleString("en-us", dateOptions);
+  }
+  
+  export { guessNumber, multiplicationTableBuilder, dateCalculation };
+  
