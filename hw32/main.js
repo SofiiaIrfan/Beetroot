ca0660eed52f36d46b173442436d61565671efe1
SofiiaIrfan <58609288+SofiiaIrfan@users.noreply.github.com>
2023-03-17 17:19:31 +0100

    Add files via upload

---
+++ hw32/main.js
@@ -0,0 +1,25 @@
+const isPerfect = (num) => {
+    let sum = 0;
+
+    for (let i = 0; i <= num / 2; i++) {
+        if (num % i === 0) {
+            sum += i;
+        }
+    }
+    return sum === num && sum !== 0 ? true : false;
+};
+
+// isPerfect(6);
+let arr = [];
+const minValPerfect = +prompt('Perfect Numbers\nEnter min value');
+const maxValPerfect = +prompt('Perfect Numbers\nEnter max value');
+const checkForPerfect = (minValPerfect, maxValPerfect) => {
+    for (let i = minValPerfect; i <= maxValPerfect; i++) {
+        if(isPerfect(i) === true){
+            arr.push(i);
+        }
+    }
+console.log(arr);
+};
+
+checkForPerfect(minValPerfect, maxValPerfect);
